ef66382463517f6530e26383276fe148e7c953d8
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2022-02-22 11:37:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as readme_file:
 
 setuptools.setup(
     name="spsa",
-    version="0.0.1",
+    version="0.0.2",
     description="The purpose of this package is to provide multivariable optimizers using SPSA.",
     packages=["spsa"],
-    python_requires=">=3.7",
+    python_requires=">=3.5",
     url="https://github.com/SimpleArt/spsa",
     author="Jack Nguyen",
     author_email="jackyeenguyen@gmail.com",
